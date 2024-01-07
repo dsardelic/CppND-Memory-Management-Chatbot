1644f8f6bbe2c13daf91ce0475212c5c21385ddd
dsardelic <25157560+dsardelic@users.noreply.github.com>
2024-01-07 12:36:40 +0100

    Add tracing output

--- src/graphedge.cpp
+++ src/graphedge.cpp
@@ -1,22 +1,29 @@
-#include "graphnode.h"
 #include "graphedge.h"
 
-GraphEdge::GraphEdge(int id)
-{
-    _id = id;
+#include <iostream>
+
+#include "graphnode.h"
+
+GraphEdge::GraphEdge(int id) {
+  std::cout << "DEBUG: "
+            << "GraphEdge::GraphEdge(int id)" << std::endl;
+  _id = id;
 }
 
-void GraphEdge::SetChildNode(GraphNode *childNode)
-{
-    _childNode = childNode;
+void GraphEdge::SetChildNode(GraphNode *childNode) {
+  std::cout << "DEBUG: "
+            << "GraphEdge::SetChildNode(GraphNode *childNode)" << std::endl;
+  _childNode = childNode;
 }
 
-void GraphEdge::SetParentNode(GraphNode *parentNode)
-{
-    _parentNode = parentNode;
+void GraphEdge::SetParentNode(GraphNode *parentNode) {
+  std::cout << "DEBUG: "
+            << "GraphEdge::SetParentNode(GraphNode *parentNode)" << std::endl;
+  _parentNode = parentNode;
 }
 
-void GraphEdge::AddToken(std::string token)
-{
-    _keywords.push_back(token);
+void GraphEdge::AddToken(std::string token) {
+  std::cout << "DEBUG: "
+            << "GraphEdge::AddToken(std::string token)" << std::endl;
+  _keywords.push_back(token);
 }
--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -1,60 +1,72 @@
-#include "graphedge.h"
 #include "graphnode.h"
 
-GraphNode::GraphNode(int id)
-{
-    _id = id;
+#include <iostream>
+
+#include "graphedge.h"
+
+GraphNode::GraphNode(int id) {
+  std::cout << "DEBUG: "
+            << "GraphNode::GraphNode(int id)" << std::endl;
+  _id = id;
 }
 
-GraphNode::~GraphNode()
-{
-    //// STUDENT CODE
-    ////
+GraphNode::~GraphNode() {
+  std::cout << "DEBUG: "
+            << "GraphNode::~GraphNode()" << std::endl;
+  //// STUDENT CODE
+  ////
 
-    delete _chatBot; 
+  delete _chatBot;
 
-    ////
-    //// EOF STUDENT CODE
+  ////
+  //// EOF STUDENT CODE
 }
 
-void GraphNode::AddToken(std::string token)
-{
-    _answers.push_back(token);
+void GraphNode::AddToken(std::string token) {
+  std::cout << "DEBUG: "
+            << "GraphNode::AddToken(std::string token)" << std::endl;
+  _answers.push_back(token);
 }
 
-void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
-{
-    _parentEdges.push_back(edge);
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge) {
+  std::cout << "DEBUG: "
+            << "GraphNode::AddEdgeToParentNode(GraphEdge *edge)" << std::endl;
+  _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
-{
-    _childEdges.push_back(edge);
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge) {
+  std::cout << "DEBUG: "
+            << "GraphNode::AddEdgeToChildNode(GraphEdge *edge)" << std::endl;
+  _childEdges.push_back(edge);
 }
 
 //// STUDENT CODE
 ////
-void GraphNode::MoveChatbotHere(ChatBot *chatbot)
-{
-    _chatBot = chatbot;
-    _chatBot->SetCurrentNode(this);
+void GraphNode::MoveChatbotHere(ChatBot *chatbot) {
+  std::cout << "DEBUG: "
+            << "GraphNode::MoveChatbotHere(ChatBot *chatbot)" << std::endl;
+  _chatBot = chatbot;
+  _chatBot->SetCurrentNode(this);
 }
 
-void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
-{
-    newNode->MoveChatbotHere(_chatBot);
-    _chatBot = nullptr; // invalidate pointer at source
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode) {
+  std::cout << "DEBUG: "
+            << "GraphNode::MoveChatbotToNewNode(GraphNode *newNode)"
+            << std::endl;
+  newNode->MoveChatbotHere(_chatBot);
+  _chatBot = nullptr;  // invalidate pointer at source
 }
 ////
 //// EOF STUDENT CODE
 
-GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
-{
-    //// STUDENT CODE
-    ////
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index) {
+  std::cout << "DEBUG: "
+            << "GraphNode::GetChildEdgeAtIndex(int index)" << std::endl;
+  //// STUDENT CODE
+  ////
 
-    return _childEdges[index];
+  return _childEdges[index];
 
-    ////
-    //// EOF STUDENT CODE
+  ////
+  //// EOF STUDENT CODE
 }
